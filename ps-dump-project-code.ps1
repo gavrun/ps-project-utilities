96e8b13a6a643d485876eedd14f53f046a453a80
AG <38539655+gavrun@users.noreply.github.com>
2025-06-22 18:00:47 +0100

    Added function

---
+++ ps-dump-project-code.ps1
@@ -0,0 +1,46 @@
+# Dump code from all project files of specified types to a single file
+#
+# Usage: 
+#
+# Get-ProjectDump
+# Get-ProjectDump -OutFile ".\project_dump.txt"
+# 
+function Get-ProjectDump {
+    param ( 
+        [string]$Path = ".",
+        [string]$OutFile = "project_dump.txt"
+    )
+    
+    # File extensions to include: ,"*.js" ,"*.css"
+    $fileExtensions = @("*.cs","*.cshtml","*.json","*.xml","*.html")
+    # Directories to exclude: "bin","obj","node_modules"
+    $excludeDirs = @("bin","obj")
+    $outputFile = $OutFile
+    $projectRoot = Get-Location
+
+    if (Test-Path $outputFile) {
+        Remove-Item $outputFile
+    }
+
+    Write-Host "Dumping code $($fileExtensions -join ', ') from $($projectRoot.Path)"
+    Write-Host "Excluding: $(($excludeDirs | ForEach-Object { '\'+$_ }) -join ', ')"
+
+    $excludePattern = ($excludeDirs | ForEach-Object { "\\$_\\" }) -join '|'
+
+    Get-ChildItem -Path $projectRoot -Recurse -Include $fileExtensions -File | Where-Object {
+        $_.FullName -notmatch $excludePattern
+    } | ForEach-Object {
+        $relativePath = $_.FullName.Replace($projectRoot.Path + "\", "")
+        $fileHeader = "\$relativePath"
+        # DEBUG
+        # Write-Host "Added $relativePath"
+        Add-Content -Path $outputFile -Value $fileHeader -Encoding UTF8
+        Get-Content -Path $_.FullName -Raw | Add-Content -Path $outputFile -Encoding UTF8
+    }
+
+    if (Test-Path $outputFile) {
+        Write-Host "Dumped to '$outputFile'."
+    } else {
+        Write-Host "No files found."
+    }
+}
